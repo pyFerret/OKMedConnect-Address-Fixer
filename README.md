c6c4e7f16021944bbd44bf362e11cc401757919d
pyFerret <69222489+pyFerret@users.noreply.github.com>
2022-07-09 18:37:27 -0500

    Update v1.2

---
+++ README.md
@@ -0,0 +1,5 @@
+a quick program for fixing addresses in 
+
+to run it you will need to open the directory in terminal/cmd, and type `python3 main.py [FILENAME]` where `[FILENAME]` is the path to the file that needs to be formatted. Also attached is the test file and the result of the program being run on it.
+
+If you have any inquiries or issues please let me know!
--- main.py
+++ main.py
@@ -11,15 +11,21 @@ except IndexError:
 
 start_time = time.time()
 
-regex = r"Street name: (.+?)\nHouse number: (.+?)\nCity: (.+?)\nState: (.+?)\nPostal code: (.+?)\nCountry:.+?\""
-subst = "\\g<2> \\g<1>, \\g<3>, \\g<4> \\g<5>\""
+regex_address = r"Street name: (.+?)\nHouse number: (.+?)\nCity: (.+?)\nState: (.+?)\nPostal code: (.+?)\nCountry:.+?\""
+subst_address = "\\g<2> \\g<1>, \\g<3>, \\g<4> \\g<5>\""
 
-name, ext =os.path.splitext(process_file)
+regex_bridges = r"\"?Bridges(.*?)\"?,"
+subst_bridges = "Bridges Health,"
+
+name, ext = os.path.splitext(process_file)
 
 oldFile = open(process_file, "r")
 newFile = open(name + "_FORMATTED" + ext, "w")
 
-newFile.write(re.sub(regex, subst, oldFile.read(), 0, re.DOTALL))
+write = re.sub(regex_address, subst_address, oldFile.read(), 0, re.DOTALL)
+write = re.sub(regex_bridges, subst_bridges, write, 0, re.MULTILINE)
+
+newFile.write(write)
 
 end_time = time.time()
 print("executed in", end_time - start_time, "seconds!")
