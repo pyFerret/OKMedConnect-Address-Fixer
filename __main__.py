f066606d1dcc2b127a62179ea6de22f768f11c69
pyFerret <69222489+pyFerret@users.noreply.github.com>
2022-08-13 13:55:55 -0500

    modification
    removes the top line and adds the date of the file to the final column

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import re
 import time
 import sys
 import os
-
+from datetime import datetime
 
 try:
     process_file = sys.argv[1]
@@ -20,16 +20,37 @@ regex_address = r"Street name:\s{0,}(.+?)\n" \
                 r"Country:.+?\""
 subst_address = "\\g<2> \\g<1>, \\g<3>, \\g<4> \\g<5>\""
 
-regex_bridges = r"\"?Bridges(.*?),"
-subst_bridges = "Bridges Health,"
+regex_bridges = r"\n\"?Bridges(.*?),"
+subst_bridges = "\nBridges Health,"
 
 name, ext = os.path.splitext(process_file)
+
+match = re.search(r'\d{4}-\d{2}-\d{2}', name)
+date = datetime.strptime(match.group(), '%Y-%m-%d').date()
+
+regex_date = r"(.+)\n"
+subst_date = "\\g<1>, " + str(date) + "\n"
+
+regex_topline = r"\"Billing Responsibility\"," \
+                r"\"Name of Billing Responsibility\"," \
+                r"UID," \
+                r"\"First Name\"," \
+                r"\"Last Name\"," \
+                r"\"Date of Birth\"," \
+                r"\"Date of Service\"," \
+                r"\"Pickup Facility\"," \
+                r"\"Pickup Address\"," \
+                r"\"Destination Facility\"," \
+                r"\"Destination Address\".{12}\n"
+subst_topline = ""
 
 oldFile = open(process_file, "r")
 newFile = open(name + "_FORMATTED" + ext, "w")
 
 write = re.sub(regex_address, subst_address, oldFile.read(), 0)
 write = re.sub(regex_bridges, subst_bridges, write, 0)
+write = re.sub(regex_date, subst_date, write, 0)
+write = re.sub(regex_topline, subst_topline, write, 0)
 
 newFile.write(write)
 
