64fd3f8131f4651a46574be45207d79ef598729e
pyFerret <69222489+pyFerret@users.noreply.github.com>
2023-01-03 19:13:51 -0600

    Update w/ format
    Updating the code to support the new sheet formats. also added some QOL features

--- __main__.py
+++ __main__.py
@@ -7,10 +7,34 @@ from datetime import datetime
 try:
     process_file = sys.argv[1]
 except IndexError:
-    print("No file path has been specified.\nWhich files would you like to process?")
+    print("No file path has been specified.\n"
+          "Which files would you like to process?")
     process_file = input()
 
+datecheck = True
+try:
+    file_date = sys.argv[2]
+except IndexError:
+    print("No date has been specified.\n"
+          "What date would you like to input?")
+    file_date = input()
+
+while datecheck:
+    try:
+        match = re.search(r'\d{4}-\d{2}-\d{2}', file_date)
+        date = datetime.strptime(match.group(), '%Y-%m-%d').date()
+        datecheck = False
+    except AttributeError:
+        print("Invalid Date. Please use the format 'YYYY-MM-DD'")
+        file_date = input()
+    except ValueError:
+        print("Invalid Date. Please use possible values")
+        file_date = input()
+
+
 start_time = time.time()
+
+print("processing:", end=" ")
 
 regex_address = r"Street name:\s{0,}(.+?)\n" \
                 r"House number:\s{0,}(.+?)\n" \
@@ -19,20 +43,18 @@ regex_address = r"Street name:\s{0,}(.+?)\n" \
                 r"Postal code:\s{0,}(.+?)\n" \
                 r"Country:.+?\""
 subst_address = "\\g<2> \\g<1>, \\g<3>, \\g<4> \\g<5>\""
+print("■", end="")
 
 regex_bridges = r"\n\"?Bridges(.*?),"
 subst_bridges = "\nBridges Health,"
+print("■", end="")
 
-name, ext = os.path.splitext(process_file)
-
-match = re.search(r'\d{4}-\d{2}-\d{2}', name)
-date = datetime.strptime(match.group(), '%Y-%m-%d').date()
 
 regex_date = r"(.+)\n"
 subst_date = "\\g<1>, " + str(date) + "\n"
+print("■", end="")
 
 regex_topline = r"\"Billing Responsibility\"," \
-                r"\"Name of Billing Responsibility\"," \
                 r"UID," \
                 r"\"First Name\"," \
                 r"\"Last Name\"," \
@@ -41,18 +63,34 @@ regex_topline = r"\"Billing Responsibility\"," \
                 r"\"Pickup Facility\"," \
                 r"\"Pickup Address\"," \
                 r"\"Destination Facility\"," \
-                r"\"Destination Address\".{12}\n"
+                r"\"Destination Address\"," \
+                r"\"Price Quoted\".{12}\n"
+print("■", end="")
+
 subst_topline = ""
+print("■")
+
+print("writing:", end=" ")
 
 oldFile = open(process_file, "r")
+print("■", end="")
+name, ext = os.path.splitext(process_file)
+print("■", end="")
 newFile = open(name + "_FORMATTED" + ext, "w")
+print("■", end="")
 
 write = re.sub(regex_address, subst_address, oldFile.read(), 0)
+print("■", end="")
 write = re.sub(regex_bridges, subst_bridges, write, 0)
+print("■", end="")
 write = re.sub(regex_date, subst_date, write, 0)
+print("■", end="")
 write = re.sub(regex_topline, subst_topline, write, 0)
+print("■", end="")
 
 newFile.write(write)
+print("■")
+
 
 end_time = time.time()
 print("executed in", end_time - start_time, "seconds!")
